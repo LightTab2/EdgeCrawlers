d8d0900307204ba13cd51fff7e2f33a00c7063ee
PatrykHub <zie.patryk@gmail.com>
2022-11-04 22:02:58 +0100

    delete duplicates from outcome file

--- crawler/run_this.py
+++ crawler/run_this.py
@@ -7,5 +7,15 @@ import sys
 #Przyklad uruchomienia: python3 run_this.py https://www.put.poznan.pl/
 #Wynik znajdzie się w outcome.txt
 
+def file_withot_duplicates():
+    f = open("outcome.txt", 'r')
+    file=f.read().splitlines()
+    file=list(set(file))
+    f.close()
+    f = open("outcome.txt", 'w')
+    for line in file: f.write(line + '\n')
+
 url = str(sys.argv).split()[1][1:-2]
-os.system("scrapy runspider spider.py -a allowed_domain=\""+urlparse(url).netloc+"\" -a url=\""+url+"\" > test.txt")
+os.system("scrapy runspider spider.py -a allowed_domain=\""+urlparse(url).netloc+"\" -a url=\""+url+"\"")
+
+file_withot_duplicates()
