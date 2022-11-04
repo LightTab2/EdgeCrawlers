efa168d9433d368d94bb7ac5557ab03d27a289fa
PatrykHub <zie.patryk@gmail.com>
2022-11-04 21:51:34 +0100

    better user expierience and crawling

--- crawler/spider.py
+++ crawler/spider.py
@@ -1,11 +1,13 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
+from urllib.parse import urlparse
 
 #pip install scrapy
 #Przykładowy kod, który przechodzi przez stronkę wydziałową, a nie zwraca linków związanych ze stroną polibudy i zwraca je do pliku textowego
-#scrapy runspider spider.py -a allowed_domain="www.put.poznan.pl" -a url="https://www.put.poznan.pl/pl/wladze-i-struktura/wydzialy" > test.txt
+#scrapy runspider spider.py -a allowed_domain="www.put.poznan.pl" -a url="https://www.put.poznan.pl/pl/wladze-i-struktura/wydzialy"
 class spider(scrapy.Spider):
     name="spider"
+    f = open('outcome.txt', 'w')
     def __init__(self, url='', allowed_domain='', **kwargs):
         self.start_urls = [url]
         self.allowed_domains = allowed_domain
@@ -14,5 +16,6 @@ class spider(scrapy.Spider):
     def parse(self, response):
         extractor = LinkExtractor(deny_domains = self.allowed_domains)
         links = extractor.extract_links(response)
+        f = open('outcome.txt', 'a')
         for link in links:
-            print (link.url)
+            f.write(urlparse(link.url).netloc + "\n")
