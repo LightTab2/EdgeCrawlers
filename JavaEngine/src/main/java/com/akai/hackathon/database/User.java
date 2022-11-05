a4afb9335f74ab55a8a96ee39bcf5dc72e94f2b2
Paweł Perechuda <aragok3@o2.pl>
2022-11-05 09:11:19 +0100

    Added user password hashing and password verification on login

--- JavaEngine/src/main/java/com/akai/hackathon/database/User.java
+++ JavaEngine/src/main/java/com/akai/hackathon/database/User.java
@@ -1,11 +1,14 @@
 package com.akai.hackathon.database;
 
+import lombok.Getter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+@Getter
 @Table(name = "users")
 public class User {
 
