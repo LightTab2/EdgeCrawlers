8c4e0f43f3a5bf646697daa573d111bceba818df
Szymon-Krysztopolski <94224331+Szymon-Krysztopolski@users.noreply.github.com>
2022-11-05 02:15:50 +0100

    tmp

--- JavaEngine/src/main/java/com/akai/hackathon/controller/UserController.java
+++ JavaEngine/src/main/java/com/akai/hackathon/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.akai.hackathon.controller;
 
 import com.akai.hackathon.database.UrlRepository;
+import com.akai.hackathon.database.Urls;
 import com.akai.hackathon.urlRater.TieHenClass;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/")
@@ -15,11 +17,13 @@ public class UserController {
     @Autowired
     UrlRepository repo;
     private final JSONObject json = new JSONObject();
+    Random rand = new Random();
 
     @GetMapping(value = "/checkSite", consumes = "application/json", produces = "application/json")
     String getResponse(@RequestBody Map<String, String> sentence) {
-        //String query = sentence.get("url");
-        String response = "65";
+        String url = sentence.get("url");
+        String response = String.valueOf(rand.nextInt(101));
+        repo.saveAndFlush(new Urls(url, Integer.parseInt(response)));
 //        repo.saveAndFlush(new Urls(1,"google.com",70));
 //        repo.saveAndFlush(new Urls(2,"facebook.com",30));
 //        repo.saveAndFlush(new Urls(3,"wp.pl",20));
